cbb53a1de659e65f38a17487738bb870698d75c6
ruanvfachini@gmail.com <ruanvfachini@gmail.com>
2026-05-06 08:49:28 -0300

    Tipo struct 'Todo'

--- CrowApi/main.cpp
+++ CrowApi/main.cpp
@@ -3,33 +3,57 @@
 
 #include "crow.h"
 
-int main(int argc, char **array)
-{
-	crow::SimpleApp app;
+struct Todo {
+    int id;
+    std::string description;
+    bool completed;
 
-	CROW_ROUTE(app, "/")([](){
-		auto page = crow::mustache::load_text("index.html");
+    static Todo fromJson(crow::json::rvalue $body, int id) {
+        return Todo{id, $body["description"].s(), false};
+    }
+};
 
-		return page;
-		});
+int main(int argc, char** array) {
+    std::vector<Todo> todos = {};
 
-	CROW_ROUTE(app, "/api/todos")
-	 	.methods("POST"_method)([](
-		const crow::request& req){
+    crow::SimpleApp app;
 
-		 auto x = crow::json::load(req.body);
-          if (!x)
-              return crow::response(400);
+    CROW_ROUTE(app, "/")([]() {
+        auto page = crow::mustache::load_text("index.html");
 
-		 crow::json::wvalue res;
-		 res["mensagem"] = "Olá";
+        return page;
+    });
 
-		return crow::response(200, res);
-	});
+    CROW_ROUTE(app, "/api/todos").methods("POST"_method)([&todos](const crow::request& req) {
+        auto data = crow::json::load(req.body);
+        if (!data) return crow::response(400);
 
-	app.port(18080)
-		.multithreaded()
-		.run();
+        long id = todos.size() + 1;
+        todos.push_back(Todo::fromJson(data, (int)id));
 
-	return 0;
+        crow::json::wvalue res;
+        res["mensagem"] = "Olá";
+        return crow::response(200, res);
+    });
+
+    CROW_ROUTE(app, "/api/todos")([&todos](const crow::request& req) {
+        crow::json::wvalue res;
+        crow::json::wvalue::list lista;
+
+        for (const auto& todo : todos) {
+            crow::json::wvalue item;
+            item["id"] = todo.id;
+            item["description"] = todo.description;
+            item["completed"] = todo.completed;
+
+            lista.push_back(item);
+        }
+
+        res["todos"] = std::move(lista);
+        return crow::response(200, res);
+    });
+
+    app.port(18080).multithreaded().run();
+
+    return 0;
 }
